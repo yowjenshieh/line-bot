4dcdc044b9b894c4331335d6d18962c44a817428
yowjenshieh <yoman8117@gmail.com>
2024-10-26 12:35:20 +0800

    debug version2

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from linebot.v3.messaging import (
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
-    TextMessage
+    TextMessage,
+    StickerMessage
 )
 from linebot.v3.webhooks import (
     MessageEvent,
