05b572460511bff2a4325ab26ab69da5922ccfb3
yowjenshieh <yoman8117@gmail.com>
2024-10-26 10:58:27 +0800

    change app back to older version

--- app.py
+++ app.py
@@ -54,15 +54,9 @@ def handle_message(event):
         r = '嗨'
     elif msg == '你吃飯了嗎':
         r = '還沒，有推薦的嗎？'
-
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=r)]
-            )
-        )
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(test=r)
 
 if __name__ == "__main__":
     app.run()
